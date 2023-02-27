59b2eb5c39ecc2a8d0719cf4d6c81c353f91e2f2
ymtowya <kaigendo@126.com>
2023-02-26 18:33:53 -0800

    fb

--- FIREBASE/firebase-setup.js
+++ FIREBASE/firebase-setup.js
@@ -3,10 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+// import {apiKey } from "@env";
+import {apiKey, } from "@env";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBsQlncrvTwIbqBTIXE20oYf5B1xphbrCo",
+  apiKey: apiKey,
   authDomain: "cs5520-demo-b01a8.firebaseapp.com",
   projectId: "cs5520-demo-b01a8",
   storageBucket: "cs5520-demo-b01a8.appspot.com",
